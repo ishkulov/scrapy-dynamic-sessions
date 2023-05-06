05f4cbce4c46377d97c6565c402bf9f789d729bf
Ilarion Ishkulov <ishkulov@lab50.net>
2023-05-06 13:03:43 +0000

    Chrome headers

--- scrapy_dynamic_sessions/objects.py
+++ scrapy_dynamic_sessions/objects.py
@@ -224,15 +224,23 @@ class Profile():
     def random_profile(cls, proxies, ua, reuse_proxy=False):
         proxy = proxies.get_random_proxy(reuse_proxy)
         user_agent = ua.get_random_ua()
-        headers = {
+
+        headers_chrome = {
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
             "Accept-Encoding": "gzip, deflate", 
             "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7", 
             "Dnt": "1", 
             "Upgrade-Insecure-Requests": "1", 
         }
+
+        headers_firefox =  {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", 
+            "Accept-Encoding": "gzip, deflate", 
+            "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3", 
+            "Upgrade-Insecure-Requests": "1", 
+        }
         
-        return cls(proxy, user_agent, headers) 
+        return cls(proxy, user_agent, headers_chrome) 
 
 class Profiles(object):
     """Controls profile storage and rotation. Rotation is random"""
